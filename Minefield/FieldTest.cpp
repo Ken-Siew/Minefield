bff53695346ab0bb5b7d238c682cb52809034705
Ken-Siew <kensiew@hotmail.com>
2016-09-23 16:01:55 -0700

    add test cases

---
+++ Minefield/FieldTest.cpp
@@ -0,0 +1,25 @@
+/**
+ * Unit Tests for Field class
+**/
+
+#include <gtest/gtest.h>
+#include <iostream>
+#include "Field.h"
+ 
+class FieldTest : public ::testing::Test
+{
+	protected:
+		FieldTest(){}
+		virtual ~FieldTest(){}
+		virtual void SetUp(){}
+		virtual void TearDown(){}
+};
+
+TEST(FieldTest, placeMineInBounds)
+{
+	Field minefield;
+	
+	minefield.placeMine(4,5);
+	ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
+  ASSERT_TRUE( minefield.placeMine(col, row) );
+}
